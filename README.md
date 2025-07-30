3d2e42a349d158042b2159d125724262d337f2b5
JaiminRaval <siliconvalley.jaimin@gmail.com>
2025-07-30 23:37:29 +0530

    initial commit
    added few files

---
+++ README.md
@@ -0,0 +1,18 @@
+# AlgoForge-CPP
+
+Welcome to **DSAlgo-py** – your one-stop repository for mastering Data Structures and Algorithms (DSA) in python!
+
+## About
+
+This repository is a curated collection of python+ implementations for classic and advanced Data Structures and Algorithms. Whether you’re preparing for coding interviews, competitive programming, or just want to strengthen your DSA skills, **DSAlgo-py** is designed to be your go-to resource.
+
+**We are learning Data Structures from the first principles thinking. so don't assume any thing**
+
+## Slides
+Google slides link:
+
+Notion link(s):
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ week-1/bubble-sort.py
@@ -0,0 +1,30 @@
+# Bubble Sort BrutForce for our class-1
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+
+        for j in range(0, n - i - 1):
+
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+# Example usage
+numbers = [64, 34, 25, 12, 22, 11, 90]
+print("Original array: ", numbers)
+sorted_numbers = bubble_sort(numbers.copy())
+print("Sorted array: ", sorted_numbers)
+
+
+# one another slightly optimized solution:
+def bubble_sort_opt(arr, target):
+
+    n = len(arr)
+
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if arr[i] + arr[j] == target:
+                return [i, j]
+    return []
