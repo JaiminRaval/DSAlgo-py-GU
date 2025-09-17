50414acda988e76a7770b0ac29c6454b8e2c46c5
JaiminRaval <siliconvalley.jaimin@gmail.com>
2025-09-18 00:47:20 +0530

    added new LC Solution

--- week-8/LeetCode-121.py
+++ week-8/LeetCode-121.py
@@ -14,3 +14,20 @@ class Solution(object):
             r += 1
 
         return maxProfit
+
+
+#  Two Pointers: (No TLE on LC)
+#
+#
+    def maxProfit_TwoPointer(self, prices):
+        l, r = 0, 1
+        maxProfit = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxProfit = max(maxProfit, profit)
+            else:
+                l = r
+            r += 1
+        return maxProfit
